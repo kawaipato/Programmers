dda285807c4b8c87308e7e50eca0d466298ed2db
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2023-08-31 16:19:36 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,7 +1,4 @@
 from collections import Counter
+from functools import reduce
 def solution(clothes):
-    clothes = [cloth[1] for cloth in clothes]
-    wow, ans = list(Counter(clothes).values()), 1
-    for w in wow:
-        ans *= w + 1
-    return ans - 1
+    return reduce(lambda x,y:x*y,[x+1 for x in Counter([cloth[1] for cloth in clothes]).values()],1)-1
