95e87c3434c27f85938237b0d39eb324fda59ea9
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2023-08-31 16:47:19 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,3 +1,7 @@
+# select i.animal_id, i.animal_type, i.name from animal_ins i inner join animal_outs o on (i.animal_id = o.animal_id)
+# where i.sex_upon_intake != o.sex_upon_outcome
+# order by i.animal_id
+
 select i.animal_id, i.animal_type, i.name from animal_ins i inner join animal_outs o on (i.animal_id = o.animal_id)
-where i.sex_upon_intake != o.sex_upon_outcome
-order by i.animal_id
+where i.sex_upon_intake like 'Intact%' and o.sex_upon_outcome not like 'Intact%'
+order by i.animal_id;
