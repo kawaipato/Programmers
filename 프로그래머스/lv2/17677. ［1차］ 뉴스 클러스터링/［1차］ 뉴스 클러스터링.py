8d20997bbdee8013d3bb4818ae5f02a5583818f1
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2023-09-04 21:18:58 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.18 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,15 +1,11 @@
 def solution(str1, str2):
     str1 = [str1[i:i+2].lower() for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
     str2 = [str2[i:i+2].lower() for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
-    if str1 > str2:
-        str1, str2 = str2, str1
-    str2_copy = str2.copy()
-    it = []
-    for s in str1:
-        if s in str2_copy:
-            it.append(s)
-            str2_copy.remove(s)
-    if len(str1 + str2) == 0:
+    intersection = set(str1) & set(str2)
+    union = set(str1) | set(str2)
+    if len(union) == 0:
         return 65536
-    else:
-        return int(65536 * (len(it)/(len(str1) + len(str2) - len(it))))
+    inte_sum = sum([min(str1.count(i), str2.count(i)) for i in intersection])
+    uni_sum = sum([max(str1.count(u), str2.count(u)) for u in union])
+    
+    return int(inte_sum/uni_sum * 65536)
