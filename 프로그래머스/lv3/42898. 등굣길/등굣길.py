b1b34027e3cbfdb934b21b30f755de0a232f74ff
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2023-09-06 19:38:24 +0900

    [level 3] Title: 등굣길, Time: 2.45 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -1,9 +1,8 @@
-import numpy as np
 def solution(m, n, puddles):
     dp = [[0] * m for _ in range(n)]
     dp[0][0] = 1
-    for i in range(n):
-        for j in range(m):
-            if [i,j] != [0,0] and not [j+1,i+1] in puddles:
-                dp[i][j] = dp[i][j-1] + dp[i-1][j]
+    for y in range(n):
+        for x in range(m):
+            if [x,y] != [0,0] and not [x+1,y+1] in puddles:
+                dp[y][x] = dp[y-1][x] + dp[y][x-1]
     return dp[-1][-1] % 1000000007
