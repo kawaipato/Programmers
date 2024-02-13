450bef8cd11227e6fec3d8312bd0bc4067a3877f
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2024-02-13 22:04:58 +0900

    [level 1] Title: 기사단원의 무기, Time: 1.97 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,17 @@
+def solution(number, limit, power):
+    def Divisor(num):
+        wow = 0
+        for i in range(1,int(num**(1/2))+1):
+            if num % i == 0:
+                wow += 1
+                if (i**2) != num:
+                    wow += 1
+        return wow
+    ans = 0
+    for n in range(1,number+1):
+        a = Divisor(n)
+        if a > limit:
+            ans += power
+        else:
+            ans += a
+    return ans
