31e31cf7c52d35919484048ce6719513c55dbff8
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2024-02-15 00:41:13 +0900

    [level 2] Title: 타겟 넘버, Time: 1.64 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,5 @@
+from itertools import product
 def solution(numbers, target):
-    K = (sum(numbers) - target)//2
-    numL = len(numbers)
-    def dfs(target, idx, cnt):
-        for i in range(idx, numL):
-            temp = target
-            temp -= numbers[i]
-            if temp == 0:
-                cnt += 1
-            elif temp > 0:
-                cnt += dfs(temp, i+1, 0)
-            else:
-                continue
-        return cnt
-    
-    return dfs(K, 0, 0)
-        
+    l = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*l)))
+    return s.count(target)
