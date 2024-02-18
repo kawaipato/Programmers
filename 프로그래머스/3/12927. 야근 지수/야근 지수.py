f9e64780b1c9dd28e394d80588dd758d169d5321
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2024-02-19 01:02:19 +0900

    [level 3] Title: 야근 지수, Time: 382.38 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,23 +1,12 @@
+import heapq
 def solution(n, works):
-    from collections import Counter
-    lst = [list(item) for item in sorted(Counter(works).items())]
-    while n > 0:
-        time, amount = lst.pop(-1)
-        if n >= amount:
-            if lst and lst[-1][0] == time - 1:
-                lst[-1][1] += amount
-            else:
-                lst.append([time-1,amount])
-            n -= amount
-        else:
-            if lst and lst[-1][0] == time - 1:
-                lst[-1][1] += n
-            else:
-                lst.append([time-1,n])
-            lst.append([time,amount-n])
-            n -= amount
-    if lst[-1][0] <= 0: return 0
-    ans = 0
-    for time, amount in lst:
-        ans += amount * time ** 2
-    return ans
+    if sum(works) <= n:
+        return 0
+    works = [ -work for work in works]
+    heapq.heapify(works)
+    for _ in range(n):
+        work = heapq.heappop(works) + 1
+        heapq.heappush(works,work)
+        
+    return sum([work ** 2 for work in works])
+    
