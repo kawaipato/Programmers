e9d8d077017868f88616401738591334e5cb2c2d
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2024-03-18 23:53:22 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 77.56 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.py
@@ -1,18 +1,15 @@
 def solution(arr):
-    def quad_tree(arr, a, n):
-        x, y, number = a[0], a[1], arr[a[0]][a[1]]
-        result = [0, 0]
+    def quad_tree(x, y, n):
+        first = arr[x][y]
         for i in range(n):
             for j in range(n):
-                if number != arr[x+i][y+j]:
-                    r1 = quad_tree(arr,[x,y],n//2)
-                    r2 = quad_tree(arr,[x+n//2,y],n//2)
-                    r3 = quad_tree(arr,[x,y+n//2],n//2)
-                    r4 = quad_tree(arr,[x+n//2,y+n//2],n//2)
-                    result = [r1[i]+r2[i]+r3[i]+r4[i] for i in range(2)]
-                    return result
-        result[number] += 1
-        return result
-    a = [0,0]
-    res = quad_tree(arr, a, len(arr))
-    return res
+                if first != arr[x+i][y+j]:
+                    quad_tree(x,y,n//2)
+                    quad_tree(x+n//2,y,n//2)
+                    quad_tree(x,y+n//2,n//2)
+                    quad_tree(x+n//2,y+n//2,n//2)
+                    return
+        ans[first] += 1
+    ans = [0,0]
+    quad_tree(0, 0, len(arr))
+    return ans
