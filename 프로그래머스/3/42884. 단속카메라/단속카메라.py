d1e338b37b70ed992e8710ae76685925c38e00b2
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2024-03-19 23:43:29 +0900

    [level 3] Title: 단속카메라, Time: 2.79 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,7 +1,7 @@
 def solution(routes):
     routes.sort(key = lambda x: (x[0],x[1]))
     now, answer = routes[0][1], 0
-    for route in routes[1:]:
+    for route in routes:
         if route[0] > now:
             now = route[1]
             answer += 1
