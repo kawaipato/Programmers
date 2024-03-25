ff636051f5d9784c556b5fb7d9324185a9831eed
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2024-03-26 02:17:20 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,14 +1,12 @@
+answer = 0
 def solution(begin, target, words):
-    def dfs(now, step, visited):
-        if now == target:
-            return step
-        min_step = float('inf')
-        for word in words:
-            if word not in visited and sum(a != b for a, b in zip(now, word)) == 1:
-                visited.add(word)
-                min_step = min(min_step, dfs(word, step + 1, visited))
-                visited.remove(word)
-        return min_step
-
-    visited = set()
-    return dfs(begin, 0, visited) if target in words else 0
+    def dfs(begin, target, words, cnt):
+        global answer
+        if begin == target:
+            answer = cnt
+        for idx, word in enumerate(words):
+            if sum(a != b for a, b in zip(begin,word)) == 1:
+                new_words = words[:idx] + words[idx+1:]
+                dfs(word, target, new_words, cnt + 1)
+    dfs(begin,target,words,0)            
+    return answer
