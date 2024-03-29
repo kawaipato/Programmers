9be51f5520262723b7f76a7ba9aeae1d2184f830
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2024-03-29 23:40:41 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,21 +1,15 @@
 def solution(genres, plays):
-    music, idx, answer, full = {}, 0, [], []
-    
-    # 각 장르에 대한 정보를 딕셔너리에 저장
+    music,idx,answer, full = {},0,[], []
     for genre, play in zip(genres, plays):
         if genre not in music:
             music[genre] = 0
         music[genre] += play
-        
-    # 장르별 총 재생 횟수를 기준으로 내림차순 정렬
     sorted_music = sorted(music.items(), key=lambda x: x[1], reverse=True)
-    
-    # 각 장르별로 최대 2곡씩 베스트 앨범에 추가
     for genre, _ in sorted_music:
         tmp = []
-        for i, (g, p) in enumerate(zip(genres, plays)):
-            if g == genre:
-                tmp.append((i, p))
-        tmp = sorted(tmp, key=lambda x: (-x[1], x[0]))  # 재생 횟수가 동일할 때 고유 번호로 정렬
-        answer.extend([t[0] for t in tmp[:2]])  # 최대 2곡 추가
+        for idx, (g, p) in enumerate(zip(genres,plays)):
+            if genre == g:
+                tmp.append((idx,p))
+        tmp = sorted(tmp, key = lambda x : (-x[1],x[0]))
+        answer += [t[0] for t in tmp[:2]]
     return answer
