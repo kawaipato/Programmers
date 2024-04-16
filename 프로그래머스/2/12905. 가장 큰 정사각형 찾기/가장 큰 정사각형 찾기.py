6e415efa7f57dbeb4cda2192c9eab0a26616a41f
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2024-04-16 15:26:23 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 417.69 ms, Memory: 30.9 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -1,12 +1,10 @@
 def solution(board):
     if not board:   return 0
-    
-    u, v = len(board), len(board[0])
     max_length = 0
-    dp = [[0] * (v+1) for _ in range(u+1)]
-    for i in range(1,u+1):
-        for j in range(1,v+1):
-            if board[i-1][j-1] == 1:
-                dp[i][j] = min(dp[i-1][j-1],dp[i][j-1],dp[i-1][j]) + 1
-                max_length = max(max_length, dp[i][j])
+    for i in range(1,len(board)):
+        for j in range(1,len(board[0])):
+            if board[i][j] >= 1:
+                board[i][j] += min(board[i-1][j-1],board[i-1][j],board[i][j-1])
+    for b in board: 
+        if max_length < max(b): max_length = max(b)
     return max_length ** 2
