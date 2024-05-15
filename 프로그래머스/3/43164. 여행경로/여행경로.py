f6541a079029cffe7717d71a7e16a5d8d9ec92a2
Seo leehwan <128393917+kawaipato@users.noreply.github.com>
2024-05-15 15:20:05 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,34 +1,16 @@
 from collections import defaultdict # 빈 key값에 빈 리스트 할당
 def solution(tickets):
-    def mapping(tickets):
-        mapp = defaultdict(list)
-        for i in range(len(tickets)):
-            a, b = tickets[i]
-            mapp[a].append((b,i))
-        for city in mapp:
-            mapp[city].sort()
-        return mapp
-    
-    MAP = mapping(tickets)
-    result = []
-    visited = [False] * len(tickets)
-    done = len(tickets) + 1
-    def dfs(city, path):
-        nonlocal done, visited, MAP, result
-        if result:
-            return
-        if len(path) == done:
-            result = path.copy()
-            return
-        for new_city, new_i in MAP[city]:
-            if visited[new_i]:
-                continue
-            visited[new_i] = True
-            path.append(new_city)
-            dfs(new_city,path)
-            path.pop()
-            visited[new_i] = False
-            
-        
-    dfs('ICN',['ICN'])
-    return result
+    MAP = defaultdict(list)
+    for a, b in tickets:
+        MAP[a].append(b)
+    for m in MAP.keys():
+        MAP[m].sort()
+
+    path, res = ["ICN"], []
+    while path:
+        now = path[-1]
+        if MAP[now]:
+            path.append(MAP[now].pop(0))
+        else:
+            res.append(path.pop())
+    return res[::-1]
